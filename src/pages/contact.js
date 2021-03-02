f5eec752fbfc9e4daa47f735fd6919e6cf18507d
Lane Garner <b.lane.garner@gmail.com>
2021-03-01 23:21:39 -0600

    recaptcha test 3

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,30 +3,30 @@ import ReCAPTCHA from "react-google-recaptcha"
 import Layout from "../components/layout"
 
 const ContactFormPage = () => (
-<Layout>
-    <div className="flex-center">
-        <h1>Contact</h1>
-        <form name="Contact Form" method="POST" data-netlify="true" data-netlify-recaptcha="true" action="/thank-you">
-        <input type="hidden" name="form-name" value="Contact Form" />
-        <div>
-            <label htmlFor="name">Name:</label><br />
-            <input type="text" id="name" name="name" />
-        </div>
-        <div>
-            <label htmlFor="email">Email:</label><br />
-            <input type="email" id="email" name="email" />
-        </div>
-        <div>
-            <label htmlFor="message">Message:</label><br />
-            <textarea id="message" name="message" />
-        </div>
-        <ReCAPTCHA sitekey={process.env.GATSBY_RECAPTCHA_KEY} />
+    <Layout>
         <div className="flex-center">
-            <button type="submit">Send</button>
+            <h1>Contact</h1>
+            <form name="Contact Form" method="POST" data-netlify="true" data-netlify-recaptcha="true" action="/thank-you">
+            <input type="hidden" name="form-name" value="Contact Form" />
+            <div>
+                <label htmlFor="name">Name:</label><br />
+                <input type="text" id="name" name="name" />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label><br />
+                <input type="email" id="email" name="email" />
+            </div>
+            <div>
+                <label htmlFor="message">Message:</label><br />
+                <textarea id="message" name="message" />
+            </div>
+            <ReCAPTCHA sitekey="GATSBY_RECAPTCHA_KEY=6Ldh8G0aAAAAANhuUBd_gXIFw0yX2LlgtyNRMk2j" />
+            <div className="flex-center">
+                <button type="submit">Send</button>
+            </div>
+            </form>
         </div>
-        </form>
-    </div>
-</Layout>
+    </Layout>
 )
 
 export default ContactFormPage
