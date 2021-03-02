b8475fd27463a1f19f10130bd099b05d911ade8c
Lane Garner <b.lane.garner@gmail.com>
2021-03-01 22:37:57 -0600

    form test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,30 @@
 import React from "react"
+import ReCAPTCHA from "react-google-recaptcha"
 import Layout from "../components/layout"
 
 const ContactFormPage = () => (
 <Layout>
-    <h1>Contact</h1>
-
-    <form name="Contact Form" method="POST" data-netlify="true">
-    <input type="hidden" name="form-name" value="Contact Form" />
-    <div>
-        <label>Email:</label>
-        <input type="email" name="email" />
+    <div className="flex-center">
+        <h1>Contact</h1>
+        <form name="Contact Form" method="POST" data-netlify="true" data-netlify-recaptcha="true" action="/thank-you">
+        <input type="hidden" name="form-name" value="Contact Form" />
+        <div>
+            <label htmlFor="name">Name:</label><br />
+            <input type="text" id="email" name="email" />
+        </div>
+        <div>
+            <label htmlFor="email">Email:</label><br />
+            <input type="email" id="email" name="email" />
+        </div>
+        <div>
+            <label htmlFor="message">Message:</label><br />
+            <textarea id="message" name="message" />
+        </div>
+        <div className="flex-center">
+            <button type="submit">Send</button>
+        </div>
+        </form>
     </div>
-    <div>
-        <label>Name:</label>
-        <input type="email" name="email" />
-    </div>
-    <div>
-        <label>Message:</label>
-        <textarea name="message" />
-    </div>
-    <button type="submit">Send</button>
-    </form>
 </Layout>
 )
 
---
+++ src/pages/thank-you.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ThankYouPage = () => (
+<Layout>
+    <div className="flex-center">
+        <h3>Thank you!</h3>
+    </div>
+</Layout>
+)
+
+export default ThankYouPage
