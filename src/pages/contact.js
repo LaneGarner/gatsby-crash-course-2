6b1e0263c1a3d13d8841066e9fee45e2b96dc156
Lane Garner <b.lane.garner@gmail.com>
2021-03-01 22:05:27 -0600

    form test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,27 @@
-import React from 'react'
+import React from "react"
 import Layout from "../components/layout"
 
-const Contact = () => {
-    return (
-        <Layout>
-            <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
-                <input type="hidden" name="bot-field" />
-                <input type="hidden" name="form-name" value="contact" />
-            {/* <form method="POST" data-netlify="true" className="flex-center"> */}
-                <div class="field">
-                    <label htmlFor="nameInput">Name</label>
-                    <input id="nameInput" name="nameInput" type="text" placeholder="name"/>
-                </div>
-                <div class="field">
-                    <label htmlFor="emailInput">Name</label>
-                    <input id="emailInput" name="emailInput" type="email" placeholder="email@address.com"/>
-                </div>
-                <div class="field">
-                    <label htmlFor="messageInput">Message</label>
-                    <textarea id="messageInput" name="messageInput" placeholder="message" minRows="2" />
-                </div>
-                <div class="field">
-                    <label htmlFor="fileInput">File</label>
-                    <input type="file" id="fileInput" name="fileInput" placeholder="Upload file" />
-                </div>
-                <div className="field">
-                    <div data-netlify-recaptcha="true"></div>
-                </div>
-                
-                <button type="submit">Submit</button>
-            </form>
-        </Layout>
-    )
-}
+const ContactFormPage = () => (
+<Layout>
+    <h1>Contact</h1>
 
-export default Contact
+    <form name="Contact Form" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="Contact Form" />
+    <div>
+        <label>Email:</label>
+        <input type="email" name="email" />
+    </div>
+    <div>
+        <label>Name:</label>
+        <input type="email" name="email" />
+    </div>
+    <div>
+        <label>Message:</label>
+        <textarea name="message" />
+    </div>
+    <button type="submit">Send</button>
+    </form>
+</Layout>
+)
+
+export default ContactFormPage
