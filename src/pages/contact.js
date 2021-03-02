8bf9f250bcf5dfedc03e5325d99a9765233a7166
Lane Garner <b.lane.garner@gmail.com>
2021-03-02 01:22:53 -0600

    recap test 15

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ const ContactFormPage = () => (
     <Layout>
         <div className="flex-center">
             <h1>Contact</h1>
-            <form name="feedback" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+            <form name="feedback" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="feedback" />
             <div>
                 <label htmlFor="name">Name</label><br />
