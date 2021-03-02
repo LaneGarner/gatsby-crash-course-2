690615ad7d629c9dd34dc6a7b29e7cb3ee7bc9f0
Lane Garner <b.lane.garner@gmail.com>
2021-03-02 01:09:01 -0600

    recap test 12

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,15 +9,15 @@ const ContactFormPage = () => (
             <form name="contact-form" method="POST" data-netlify="true" data-netlify-recaptcha="true">
             <input type="hidden" name="form-name" value="contact-form" />
             <div>
-                <label htmlFor="name">Name:</label><br />
+                <label htmlFor="name">Name</label><br />
                 <input type="text" id="name" name="name" />
             </div>
             <div>
-                <label htmlFor="email">Email:</label><br />
+                <label htmlFor="email">Email</label><br />
                 <input type="email" id="email" name="email" />
             </div>
             <div>
-                <label htmlFor="message">Message:</label><br />
+                <label htmlFor="message">Message</label><br />
                 <textarea id="message" name="message" />
             </div>
             <div>
